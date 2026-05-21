6c72870c8901bd0b12f44e8053c7415bcfcc6432
Pratyushhhd <20810418BCA@Prime.edu.np>
2026-05-21 20:54:21 +0545

    Add providers component

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'react-hot-toast';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      {children}
+      <Toaster position="top-right" />
+    </ThemeProvider>
+  );
+}
